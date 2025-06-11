a96707fb05cf92261af72cd0cde5cfb6eb761542
Gilberto Mascena <gilbertomascena@gmail.com>
2025-06-11 18:31:13 -0300

    :sparkles: feat: Updated Repository

--- src/main/java/br/com/mascenadev/crud/repository/TodoRepository.java
+++ src/main/java/br/com/mascenadev/crud/repository/TodoRepository.java
@@ -1,8 +1,7 @@
 package br.com.mascenadev.crud.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import br.com.mascenadev.crud.domain.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
