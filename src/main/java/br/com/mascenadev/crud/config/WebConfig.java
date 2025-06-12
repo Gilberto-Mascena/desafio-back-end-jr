ce5906e50457e3fdc6085a659d2679d2abf06c2a
Gilberto Mascena <gilbertomascena@gmail.com>
2025-06-12 19:41:32 -0300

    :books: Added Javadoc class WebConfig

--- src/main/java/br/com/mascenadev/crud/config/WebConfig.java
+++ src/main/java/br/com/mascenadev/crud/config/WebConfig.java
@@ -4,9 +4,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Configuração web global para a aplicação.
+ * Esta classe implementa {@link WebMvcConfigurer} para customizar a configuração padrão do Spring MVC,
+ * especificamente para configurar o CORS (Compartilhamento de Recursos de Origem Cruzada).
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    /**
+     * Configura o CORS (Cross-Origin Resource Sharing) para a aplicação.
+     * Este método define as regras para quais origens são permitidas acessar os recursos
+     * e quais métodos HTTP e cabeçalhos são permitidos para caminhos específicos.
+     *
+     * @param registry O {@link CorsRegistry} ao qual os mapeamentos CORS são adicionados.
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/todos/**")
