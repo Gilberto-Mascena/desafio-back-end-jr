251740dd372eb512dcf23c9e70b0368909b55aa1
Gilberto Mascena <gilbertomascena@gmail.com>
2025-06-12 11:33:51 -0300

    :recycle: CrudApplicationTests

--- src/test/java/br/com/mascenadev/crud/CrudApplicationTests.java
+++ src/test/java/br/com/mascenadev/crud/CrudApplicationTests.java
@@ -2,47 +2,11 @@ package br.com.mascenadev.crud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.reactive.server.WebTestClient;
 
-import br.com.mascenadev.crud.domain.Todo;
-import jakarta.annotation.Resource;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 class CrudApplicationTests {
 
-	@Resource
-	private WebTestClient webTestClient;
-
-	@Test
-	void testCreateTodoSuccess() {
-
-		var todo = new Todo("Tudo 6", "Tudo 6", true, 1);
-
-		webTestClient
-				.post()
-				.uri("/todos")
-				.bodyValue(todo)
-				.exchange()
-				.expectStatus().isOk()
-				.expectBody()
-				.jsonPath("$").isArray()
-				.jsonPath("$.length()").isEqualTo(1)
-				.jsonPath("$[0].nome").isEqualTo("Tudo 6")
-				.jsonPath("$[0].descricao").isEqualTo(todo.getNome())
-				.jsonPath("$[0].realizado").isEqualTo(todo.isRealizado())
-				.jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
-	}
-
-	@Test
-	void testCreateTodoFail() {
-
-		webTestClient
-				.post()
-				.uri("/todos")
-				.bodyValue(
-						new Todo("", "", true, 1))
-				.exchange()
-				.expectStatus()
-				.isBadRequest();
-	}
+    @Test
+    void contextLoads() {
+    }
 }
