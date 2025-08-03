eec8446974ddf58bd0633f9076f4988c012c6d54
Gilberto Mascena <gilbertomascena@gmail.com>
2025-08-03 15:21:36 -0300

    :recycle: refactor: Updating imports in the TodoRequestDTO class

--- src/main/java/br/com/mascenadev/crud/dtos/TodoRequestDTO.java
+++ src/main/java/br/com/mascenadev/crud/dtos/TodoRequestDTO.java
@@ -1,7 +1,11 @@
 package br.com.mascenadev.crud.dtos;
 
 import br.com.mascenadev.crud.domain.Todo;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.beans.BeanUtils;
 
 /**
