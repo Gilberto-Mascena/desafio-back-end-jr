29c40d197588ec3cea00b851dd5c47da5137bb8e
Gilberto Mascena <gilbertomascena@gmail.com>
2025-08-03 15:20:26 -0300

    :recycle: refactor: Updating imports in the Todo class

--- src/main/java/br/com/mascenadev/crud/domain/Todo.java
+++ src/main/java/br/com/mascenadev/crud/domain/Todo.java
@@ -1,7 +1,16 @@
 package br.com.mascenadev.crud.domain;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.Objects;
 
