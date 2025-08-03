e7141f2c07ee1dc4b2662b8892f5f7948b7f8a66
Gilberto Mascena <gilbertomascena@gmail.com>
2025-08-03 15:19:38 -0300

    :recycle: refactor: Updating imports in the TodoController class

--- src/main/java/br/com/mascenadev/crud/controller/TodoController.java
+++ src/main/java/br/com/mascenadev/crud/controller/TodoController.java
@@ -5,7 +5,14 @@ import br.com.mascenadev.crud.dtos.TodoResponseDTO;
 import br.com.mascenadev.crud.service.TodoService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 import java.util.List;
